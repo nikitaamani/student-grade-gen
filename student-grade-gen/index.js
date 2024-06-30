e93a4ffd1e3e6b121eed8b119261b2015495e79e
nikita <nikita.bosire@student.moringaschool.com>
2024-06-30 18:22:07 +0300

    initial commit

--- student-grade-gen/index.js
+++ student-grade-gen/index.js
@@ -12,18 +12,5 @@ function calculateGrade(mark) {
     }
   }
    
-  function studentGradeGenerator() {
-    const mark = parseFloat(prompt('Enter the student mark (0-100):'));
   
-    if (isNaN(mark) || mark < 0 || mark > 100) {
-      return 'Invalid input. Please enter a valid mark between 0 and 100.';
-    }
-  
-    const grade = calculateGrade(mark);
-    return `The student's grade is: ${grade}`;
-  }
-  
-  
-  const result = studentGradeGenerator();
-  console.log(result);
   
